59c46c3713105fc836a9af606c8e65069fcbd5b1
pavelfedor <pavelbjorn@gmail.com>
2018-07-20 16:36:42 +0300

    Small changes

--- app/src/main/java/com/sample/wrapper/realm/pavelfedor/manageconatactssample/Test.kt
+++ app/src/main/java/com/sample/wrapper/realm/pavelfedor/manageconatactssample/Test.kt
@@ -48,6 +48,86 @@ data.add(values);
 intent.putParcelableArrayListExtra(ContactsContract.Intents.Insert.DATA, data);
 
 startActivityForResult(intent, 0);
-
-
 }*/
+
+
+
+/*fun getContacts() {
+    contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+    ).apply {
+        moveToFirst()
+        while (moveToNext()) {
+            val id = getString(getColumnIndex(ContactsContract.Contacts._ID))
+            val name = getString(getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+            val data = mutableListOf<ContactsData>()
+
+            if (getInt(getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+
+                contentResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        "${ContactsContract.Data.CONTACT_ID} = ?",
+                        arrayOf(id),
+                        null,
+                        null
+                ).apply {
+                    moveToFirst()
+                    while (moveToNext()) {
+                        data.add(ContactsData(
+                                getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID)),
+                                getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.MIMETYPE)),
+                                listOf(getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)))
+                        ))
+                    }
+
+                }.close()
+
+                contentResolver.query(
+                        ContactsContract.Data.CONTENT_URI,
+                        null,
+                        "${ContactsContract.Data.CONTACT_ID} = ?",
+                        arrayOf(id),
+                        null,
+                        null
+                ).apply {
+                    moveToFirst()
+                    while (moveToNext()) {
+
+                        data.add(ContactsData(
+                                getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID)),
+                                getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.MIMETYPE)),
+                                listOf(
+                                        getString(getColumnIndex(ContactsContract.Data._ID)),
+                                        getString(getColumnIndex(ContactsContract.Data.MIMETYPE)),
+                                        getString(getColumnIndex(ContactsContract.Data.DATA1)),
+                                        getString(getColumnIndex(ContactsContract.Data.DATA6))
+                                        *//* getString(getColumnIndex(ContactsContract.Data.DATA3)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA4)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA5)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA6)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA7)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA8)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA9)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA10)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA11)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA12)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA13)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA14)),
+                                             getString(getColumnIndex(ContactsContract.Data.DATA15))*//*
+                                    )
+                            ))
+                        }
+                    }.close()
+
+                    contacts.add(Contact(id, name, data))
+                }
+            }
+        }.close()
+
+        Toast.makeText(this, "End", Toast.LENGTH_SHORT).show()
+    }*/
